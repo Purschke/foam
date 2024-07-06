ee3bf3152f4e26a2f6b78f53409b29dbd0e94960
Karl-Paul <karl-paul2@web.de>
2024-07-06 16:24:25 +0200

    added base classes

---
+++ packages/foam-vscode/src/core/model/trainNote.ts
@@ -0,0 +1,12 @@
+import { Resource } from "./note";
+
+export interface TrainNote extends Resource{
+  nextReminder: Date;
+  currentPhase: Phase;
+}
+
+export interface Phase{
+  phase: number;
+  days: number;
+}
+
