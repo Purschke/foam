7e93aabf033581fe8fd5958a3bd7e0405ce3359c
Karl-Paul <karl-paul2@web.de>
2024-07-20 10:41:44 +0200

    structured Base Clases

---
+++ packages/foam-vscode/src/core/model/phase.ts
@@ -0,0 +1,33 @@
+export class Phase {
+  name: string;
+  days: number;
+
+  constructor(name: string, days: number) {
+    this.name = name;
+    this.days = days;
+  }
+}
+
+export class Phases {
+  phases = Array<Phase>;
+
+  public First(): Phase {
+    return this.phases[0];
+  }
+
+  public Next(phase: Phase): Phase {
+    for (let i = 0; i < this.phases.length; i++) {
+      if (this.phases[i] == phase) {
+        return Phases[i++];
+      }
+    }
+  }
+
+  public Return(phase: Phase): Phase {
+    for (let i = 0; i < this.phases.length; i++) {
+      if (this.phases[i] == phase) {
+        return Phases[i--];
+      }
+    }
+  }
+}
