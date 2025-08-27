ea4ff5a2ea92e568fd010a1a18a647b4775224fc
Karl-Paul <karl-paul2@web.de>
2025-08-27 11:49:17 +0200

    test: added update and delet test

--- packages/foam-vscode/src/core/model/train-noteService.test.ts
+++ packages/foam-vscode/src/core/model/train-noteService.test.ts
@@ -4,6 +4,7 @@ import {
   createTestWorkspace,
 } from '../../test/test-utils';
 import { TrainNoteService } from './train-noteService';
+import { URI } from './uri';
 
 describe('Synced trie', () => {
   it('Added', () => {
@@ -21,5 +22,45 @@ describe('Synced trie', () => {
         .map(n => n.uri.path)
         .sort()
     ).toEqual(['/page-a.md', '/page-c.md']);
+  });
+
+  it('Updated', () => {
+    const ws = createTestWorkspace();
+    ws.set(createTestTrainNote({ uri: '/page-a.md', title: 'foo' }));
+
+    const ts = TrainNoteService.fromWorkspace(ws);
+
+    ws.set(createTestTrainNote({ uri: '/page-c.md', title: 'bar' }));
+    ws.set(createTestNote({ uri: '/page-b.md', title: 'Fred' }));
+
+    ws.set(createTestTrainNote({ uri: '/page-c.md', title: 'Mani' }));
+
+    expect(
+      ts
+        .list()
+        .map(n => ({ path: n.uri.path, title: n.title }))
+        .sort((a, b) => a.path.localeCompare(b.path))
+    ).toEqual([
+      { path: '/page-a.md', title: 'foo' },
+      { path: '/page-c.md', title: 'Mani' },
+    ]);
+  });
+
+  it('Deleted', () => {
+    const ws = createTestWorkspace();
+    ws.set(createTestTrainNote({ uri: '/page-a.md' }));
+
+    const ts = TrainNoteService.fromWorkspace(ws);
+
+    ws.set(createTestTrainNote({ uri: '/page-c.md' }));
+    ws.set(createTestNote({ uri: '/page-b.md' }));
+    ws.delete(URI.parse('/page-a.md'));
+
+    expect(
+      ts
+        .list()
+        .map(n => n.uri.path)
+        .sort()
+    ).toEqual(['/page-c.md']);
   });
 });
